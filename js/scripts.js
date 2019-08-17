3b1b6bc10314ee35a2a06ad6b8486881d09f5abe
Jordan Safford <jordansafford@gmail.com>
2019-08-16 18:55:23 -0700

    Add submit button & javascript to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-$document.ready(function() {
-  var total = parseInt();
-}
+var score = function parseInt(value1, value2, value3, value4, value5, value6)
+
+$(document).ready(function()
+  if (score <= 2) {
+    $('#result-one').show();
+  } else if (score === 3) {
+    $('result-two').show();
+  } else if ( score >= 4)
+    $('#result-three').show();
+});
